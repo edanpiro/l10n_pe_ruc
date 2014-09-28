2061893bbee06d7346d385e1cdf32e5cbf406675
Edgard Pimentel <epimentel@btg.com.pe>
2014-09-27 22:44:58 -0500

    agregando archivos iniciales

---
+++ __openerp__.py
@@ -0,0 +1,18 @@
+# coding= utf-8
+
+
+{
+    "name": "Validar RUC",
+    "version": "1.0",
+    "depends": [
+        "base"
+    ],
+    "author": "Edgard Pimentel pimentelrojas@gmail.com",
+    "website": "",
+    "category": "",
+    "description": "Validación de RUC",
+    'data': [
+    ],
+    'installable': True,
+    'active': False
+}
