f655ea56eadeaf49ebcbd63312585a9e6c58021a
Edgard Pimentel <epimentel@btg.com.pe>
2014-09-29 11:05:04 -0500

    eliminando boton validar y agregando on_change para validar ruc

--- res_partner/res_partner.py
+++ res_partner/res_partner.py
@@ -11,24 +11,25 @@ import xml.etree.ElementTree as ET
 class res_partner(osv.osv):
     _inherit = 'res.partner'
 
+    def on_change_ruc(self, cr, uid, ids, vat, context=None):
+        if len(vat) == 11:
+            link = 'http://www.sunat.gob.pe/w/wapS01Alias?ruc=%s' % vat
+            cliente = requests.get(link)
+            cliente = cliente.text.replace('\r', '').replace('\n', '').replace('\t', '')
+            root = html.fromstring(cliente)
+            print root[2].attrib
+            print root[2][0][0].text_content()
+            print root[2][0][7].text_content()
+            print root[2][0][9].text_content()
+            name = root[2][0][0].text_content().split('-')
+            return {
+                'value': {'name': name[1][1:-1]}
+            }
+        return {}
+
     _columns = {
         'vat': fields.char('TIN', size=11, help="Tax Identification Number. Check the box if this contact is subjected to taxes. Used by the some of the legal statements.")
     }
-
-    def button_check_vat(self, cr, uid, ids, context=None):
-
-        link = 'http://www.sunat.gob.pe/w/wapS01Alias?ruc=20509849316'
-        cliente = requests.get(link)
-
-        cliente = cliente.text.replace('\r', '').replace('\n', '').replace('\t', '')
-        #root = ET.fromstring(cliente)
-        root = html.fromstring(cliente)
-        print root[2].attrib
-        print root[2][0][0].text_content()
-        print root[2][0][7].text_content()
-        print root[2][0][9].text_content()
-
-        return True
 
 
 res_partner()
